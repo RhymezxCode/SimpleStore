71b1232c43b5b693bc9a4ce7becc63242b34b00a
Awodire babajide samuel <rhymezx.code@gmail.com>
2022-12-10 14:59:00 +0100

    all recent changes

--- app/src/main/java/io/github/rhymezxcode/simplestore/MainActivity.kt
+++ app/src/main/java/io/github/rhymezxcode/simplestore/MainActivity.kt
@@ -12,91 +12,91 @@ import kotlinx.coroutines.launch
 
 @RequiresApi(Build.VERSION_CODES.M)
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-
-        //share preference builder
-        val storeOne = SimpleStore.Builder()
-            .context(context = this)
-            .storeName(name = "MyStorePreference")
-            .encryption(encrypted = false)
-            .dispatcher(dispatcher = Default)
-            .build()
-
-        //Encrypted share preference builder
-        val storeTwo = SimpleStore.Builder()
-            .context(context = this)
-            .storeName(name = "MyStoreEncryptedPreference")
-            .encryption(encrypted = true)
-            .dispatcher(dispatcher = Default)
-            .build()
-
-        //datastore preference builder
-        val storeThree = SimpleStore.Builder()
-            .context(context = this)
-            .storeName(name = "MyDataStorePreference")
-            .encryption(encrypted = false)
-            .build()
-
-        //datastore preference builder
-        val storeFour = SimpleStore.Builder()
-            .context(context = this)
-            .storeName(name = "MyDataStoreEncryptedPreference")
-            .encryption(encrypted = true)
-            .build()
-
-        binding.store.setOnClickListener {
-            lifecycleScope.launch {
-                storeOne.sharedPreference()
-                    ?.saveStringToStore("name",
-                        binding.editTextTextPersonName.text.toString())
-            }
-
-            lifecycleScope.launchWhenCreated {
-                binding.sharedPreferenceValue.text =
-                    storeOne.sharedPreference()?.getStringFromStore("name")
-            }
-
-            lifecycleScope.launch {
-                storeTwo.sharedPreference()
-                    ?.saveStringToStore("name",
-                        binding.editTextTextPersonName.text.toString())
-            }
-
-            lifecycleScope.launchWhenCreated {
-                binding.sharedPreferenceEncryptedValue.text =
-                    storeOne.sharedPreference()?.getStringFromStore("name")
-            }
-
-            lifecycleScope.launch {
-                storeThree.datastorePreference()
-                    ?.saveStringToStore("name",
-                        binding.editTextTextPersonName.text.toString())
-            }
-
-            lifecycleScope.launchWhenCreated {
-                storeOne.datastorePreference()?.
-                getStringFromStore("name")?.collectLatest {
-                    binding.datastorePreferenceValue.text = it
-                }
-            }
-
-            lifecycleScope.launch {
-                storeFour.datastorePreference()
-                    ?.saveStringToStore("name",
-                        binding.editTextTextPersonName.text.toString())
-            }
-
-            lifecycleScope.launchWhenCreated {
-                storeFour.datastorePreference()?.
-                getStringFromStore("name")?.collectLatest {
-                    binding.datastorePreferencEncryptedValue.text = it
-                }
-            }
-        }
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+//
+//        //share preference builder
+//        val storeOne = SimpleStore.Builder()
+//            .context(context = this)
+//            .storeName(name = "MyStorePreference")
+//            .encryption(encrypted = false)
+//            .dispatcher(dispatcher = Default)
+//            .build()
+//
+//        //Encrypted share preference builder
+//        val storeTwo = SimpleStore.Builder()
+//            .context(context = this)
+//            .storeName(name = "MyStoreEncryptedPreference")
+//            .encryption(encrypted = true)
+//            .dispatcher(dispatcher = Default)
+//            .build()
+//
+//        //datastore preference builder
+//        val storeThree = SimpleStore.Builder()
+//            .context(context = this)
+//            .storeName(name = "MyDataStorePreference")
+//            .encryption(encrypted = false)
+//            .build()
+//
+//        //datastore preference builder
+//        val storeFour = SimpleStore.Builder()
+//            .context(context = this)
+//            .storeName(name = "MyDataStoreEncryptedPreference")
+//            .encryption(encrypted = true)
+//            .build()
+//
+//        binding.store.setOnClickListener {
+//            lifecycleScope.launch {
+//                storeOne.sharedPreference()
+//                    ?.saveStringToStore("name",
+//                        binding.editTextTextPersonName.text.toString())
+//            }
+//
+//            lifecycleScope.launchWhenCreated {
+//                binding.sharedPreferenceValue.text =
+//                    storeOne.sharedPreference()?.getStringFromStore("name")
+//            }
+//
+//            lifecycleScope.launch {
+//                storeTwo.sharedPreference()
+//                    ?.saveStringToStore("name",
+//                        binding.editTextTextPersonName.text.toString())
+//            }
+//
+//            lifecycleScope.launchWhenCreated {
+//                binding.sharedPreferenceEncryptedValue.text =
+//                    storeOne.sharedPreference()?.getStringFromStore("name")
+//            }
+//
+//            lifecycleScope.launch {
+//                storeThree.datastorePreference()
+//                    ?.saveStringToStore("name",
+//                        binding.editTextTextPersonName.text.toString())
+//            }
+//
+//            lifecycleScope.launchWhenCreated {
+//                storeOne.datastorePreference()?.
+//                getStringFromStore("name")?.collectLatest {
+//                    binding.datastorePreferenceValue.text = it
+//                }
+//            }
+//
+//            lifecycleScope.launch {
+//                storeFour.datastorePreference()
+//                    ?.saveStringToStore("name",
+//                        binding.editTextTextPersonName.text.toString())
+//            }
+//
+//            lifecycleScope.launchWhenCreated {
+//                storeFour.datastorePreference()?.
+//                getStringFromStore("name")?.collectLatest {
+//                    binding.datastorePreferencEncryptedValue.text = it
+//                }
+//            }
+//        }
+//
 
     }
 
